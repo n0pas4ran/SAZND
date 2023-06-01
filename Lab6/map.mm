862d4a25f564173835dc52c208d57edfe8157a64
n0pas4ran <ilya.tolkunov@mail.ru>
2023-06-02 00:49:31 +0300

    add 6

---
+++ Lab6/map.mm
@@ -0,0 +1,166 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Концепция и модели менеджмента безопасности&#xa;информационных и телекоммуникационных&#xa;технологийя карта" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1685117332387"><hook NAME="MapStyle" background="#2e3440" zoom="0.269">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="Область применения" POSITION="bottom_or_right" ID="ID_1518464241" CREATED="1685117639466" MODIFIED="1685117647521"/>
+<node TEXT="Термины и определения" POSITION="top_or_left" ID="ID_1013825481" CREATED="1685117650177" MODIFIED="1685117659042">
+<node TEXT=" подотчетность" ID="ID_687117598" CREATED="1685118063247" MODIFIED="1685118084859"/>
+<node TEXT="активы" ID="ID_724111996" CREATED="1685118069839" MODIFIED="1685118091997"/>
+<node TEXT=" аутентичность" ID="ID_534025019" CREATED="1685118070492" MODIFIED="1685118097997"/>
+<node TEXT="доступность" ID="ID_1726669479" CREATED="1685118070661" MODIFIED="1685118103682"/>
+<node TEXT="базовые защитные меры" ID="ID_361628222" CREATED="1685118070830" MODIFIED="1685118115682"/>
+<node TEXT="конфиденциальность" ID="ID_1619231932" CREATED="1685118070998" MODIFIED="1685118125341"/>
+<node TEXT=" контроль" ID="ID_35971826" CREATED="1685118071144" MODIFIED="1685118132811"/>
+<node TEXT=" рекомендации" ID="ID_1244542756" CREATED="1685118071324" MODIFIED="1685118140371"/>
+<node TEXT=" воздействие" ID="ID_1283856885" CREATED="1685118071493" MODIFIED="1685118147975"/>
+<node TEXT="инцидент информационной безопасности" ID="ID_1242260128" CREATED="1685118071673" MODIFIED="1685118160756"/>
+<node TEXT="безопасность информационно-телекоммуникационных технологий (безопасность ИТТ)" ID="ID_1838090346" CREATED="1685118071854" MODIFIED="1685118171599"/>
+<node TEXT="политика безопасности информационно-телекоммуникационных технологий (политика&#xa;безопасности ИТТ)" ID="ID_143920408" CREATED="1685118072033" MODIFIED="1685118184664"/>
+<node TEXT="средство(а) обработки информации" ID="ID_607711225" CREATED="1685118072190" MODIFIED="1685118193199"/>
+<node TEXT=" информационная безопасность" ID="ID_1821234608" CREATED="1685118072348" MODIFIED="1685118202903"/>
+<node TEXT=" целостность" ID="ID_1779638553" CREATED="1685118072529" MODIFIED="1685118209563"/>
+<node TEXT="неотказуемость" ID="ID_507799529" CREATED="1685118072697" MODIFIED="1685118218412"/>
+<node TEXT=" достоверность" ID="ID_1214570823" CREATED="1685118072866" MODIFIED="1685118225267"/>
+<node TEXT=" остаточный риск" ID="ID_1217850524" CREATED="1685118073023" MODIFIED="1685118243881"/>
+<node TEXT="риск" ID="ID_960031081" CREATED="1685118073204" MODIFIED="1685118250076"/>
+<node TEXT="анализ риска" ID="ID_1762856380" CREATED="1685118073360" MODIFIED="1685118260679"/>
+<node TEXT="оценка риска" ID="ID_1318723651" CREATED="1685118073552" MODIFIED="1685118266051"/>
+<node TEXT="менеджмент риска" ID="ID_722283769" CREATED="1685118073720" MODIFIED="1685118273880"/>
+<node TEXT="обработка риска" ID="ID_424325509" CREATED="1685118073889" MODIFIED="1685118280419"/>
+<node TEXT="защитная мера" ID="ID_381631868" CREATED="1685118074080" MODIFIED="1685118288534"/>
+<node TEXT="угроза" ID="ID_1790508946" CREATED="1685118074238" MODIFIED="1685118295660"/>
+<node TEXT="уязвимость" ID="ID_187649545" CREATED="1685118074418" MODIFIED="1685118302093"/>
+</node>
+<node TEXT="Концепции безопасности и взаимосвязи" POSITION="bottom_or_right" ID="ID_660307374" CREATED="1685118325619" MODIFIED="1685118415268">
+<node TEXT=" Принципы безопасности" ID="ID_392653615" CREATED="1685118342686" MODIFIED="1685118353302"/>
+<node TEXT="Активы" ID="ID_966604945" CREATED="1685118361326" MODIFIED="1685118363486"/>
+<node TEXT=" Угрозы" ID="ID_888545454" CREATED="1685118377470" MODIFIED="1685118379385"/>
+<node TEXT=" Уязвимости" ID="ID_1396112028" CREATED="1685118389382" MODIFIED="1685118390964"/>
+<node TEXT=" Воздействие" ID="ID_848377631" CREATED="1685118399619" MODIFIED="1685118401554"/>
+<node TEXT="Риск" ID="ID_1208746536" CREATED="1685118413164" MODIFIED="1685118416239"/>
+<node TEXT=" Защитные меры" ID="ID_1199224662" CREATED="1685118422781" MODIFIED="1685118424564"/>
+<node TEXT="Ограничения" ID="ID_412084178" CREATED="1685118439341" MODIFIED="1685118440942"/>
+<node TEXT="Взаимосвязь компонентов безопасности" ID="ID_887928349" CREATED="1685118448891" MODIFIED="1685118450497"/>
+</node>
+<node TEXT="Цели, стратегии и политика" POSITION="top_or_left" ID="ID_130163251" CREATED="1685118465248" MODIFIED="1685119036486" HGAP_QUANTITY="88.625 pt" VSHIFT_QUANTITY="35.25 pt">
+<node TEXT="Цели и стратегии безопасности информационно-телекоммуникационных технологий" ID="ID_1188750923" CREATED="1685118484340" MODIFIED="1685118487936"/>
+<node TEXT="Иерархия политик" ID="ID_817806350" CREATED="1685118509943" MODIFIED="1685118512295"/>
+<node TEXT=" Элементы политики безопасности информационно-телекоммуникационных технологий&#xa;организации" ID="ID_771956752" CREATED="1685118522543" MODIFIED="1685118524145"/>
+</node>
+<node TEXT="Организационные аспекты безопасности информационнотелекоммуникационных технологий" POSITION="bottom_or_right" ID="ID_961873432" CREATED="1685118551464" MODIFIED="1685118556933" HGAP_QUANTITY="4.25 pt" VSHIFT_QUANTITY="22.5 pt">
+<node TEXT="Служебные обязанности и ответственность" ID="ID_1304680396" CREATED="1685118567878" MODIFIED="1685118570403">
+<node TEXT="Служебные обязанности, подотчетность и ответственность в организации" ID="ID_404102140" CREATED="1685118579690" MODIFIED="1685118583032"/>
+<node TEXT="Совет по безопасности информационно-телекоммуникационных технологий" ID="ID_636294151" CREATED="1685118593627" MODIFIED="1685118595735"/>
+<node TEXT="Администратор безопасности информационно-телекоммуникационных технологий" ID="ID_1752808828" CREATED="1685118604529" MODIFIED="1685118606295"/>
+<node TEXT="Пользователи информационно-телекоммуникационных технологий" ID="ID_288830689" CREATED="1685118616555" MODIFIED="1685118618460"/>
+</node>
+<node TEXT="Организационные принципы" ID="ID_681876124" CREATED="1685118651137" MODIFIED="1685118697582">
+<node TEXT="Обязательства" ID="ID_1901785804" CREATED="1685118700566" MODIFIED="1685118710555"/>
+<node TEXT="Последовательный подход" ID="ID_127225074" CREATED="1685118714099" MODIFIED="1685118722810"/>
+<node TEXT=" Интегрирование безопасности информационно-телекоммуникационных технологий" ID="ID_1600276260" CREATED="1685118733583" MODIFIED="1685118735725"/>
+</node>
+</node>
+<node TEXT="Функции управления безопасностью информационнотелекоммуникационными технологиями" POSITION="top_or_left" ID="ID_912365634" CREATED="1685118763181" MODIFIED="1685119241862" HGAP_QUANTITY="-282.24999 pt" VSHIFT_QUANTITY="-88.125 pt">
+<node TEXT="Общие вопросы" ID="ID_1390649018" CREATED="1685118828733" MODIFIED="1685119069451">
+<node TEXT=" планирование" ID="ID_1201289678" CREATED="1685118842638" MODIFIED="1685118844425">
+<node TEXT="определение организацией требований по безопасности ИТТ" ID="ID_371797286" CREATED="1685118854798" MODIFIED="1685118856635"/>
+<node TEXT="определение организацией целей, стратегий и политик безопасности ИТТ" ID="ID_1463930807" CREATED="1685118861648" MODIFIED="1685118869070"/>
+<node TEXT="установление обязанностей и ответственности в рамках организации" ID="ID_971579190" CREATED="1685118879906" MODIFIED="1685118885733"/>
+<node TEXT=" разработка плана по безопасности ИТТ" ID="ID_1253687576" CREATED="1685118893518" MODIFIED="1685118895407"/>
+<node TEXT=" оценка рисков" ID="ID_1930472320" CREATED="1685118901235" MODIFIED="1685118903539"/>
+<node TEXT="решение об обработке риска и выборе защитных мер" ID="ID_1556894589" CREATED="1685118913710" MODIFIED="1685118918732"/>
+<node TEXT="планирование непрерывности бизнеса" ID="ID_392806859" CREATED="1685118926018" MODIFIED="1685118927551"/>
+</node>
+<node TEXT=" реализация" ID="ID_891571100" CREATED="1685118937314" MODIFIED="1685118939356">
+<node TEXT=" создание защитных мер" ID="ID_974111864" CREATED="1685118948619" MODIFIED="1685118951716"/>
+<node TEXT="одобрение ИТТ" ID="ID_1716706608" CREATED="1685118954298" MODIFIED="1685118962050"/>
+<node TEXT="разработка и выполнение программы осведомленности персонала о безопасности," ID="ID_1638134502" CREATED="1685118971623" MODIFIED="1685118976675"/>
+<node TEXT="аудит-функционирование защитных мер" ID="ID_100389151" CREATED="1685118995157" MODIFIED="1685119000973"/>
+</node>
+<node TEXT="эксплуатация и поддержка" ID="ID_670752844" CREATED="1685119013010" MODIFIED="1685119069451" HGAP_QUANTITY="-70.75 pt" VSHIFT_QUANTITY="18 pt">
+<node TEXT="контроль конфигурации и управление изменениями" ID="ID_788295944" CREATED="1685119061227" MODIFIED="1685119062921"/>
+<node TEXT="управление непрерывностью бизнеса" ID="ID_1432135723" CREATED="1685119071789" MODIFIED="1685119079390"/>
+<node TEXT="анализ, аудит и мониторинг, а также проверка соответствия безопасности заявленным требованиям" ID="ID_1020525740" CREATED="1685119086841" MODIFIED="1685119088719"/>
+<node TEXT="управление инцидентами безопасности информации" ID="ID_575467550" CREATED="1685119103164" MODIFIED="1685119104873"/>
+</node>
+</node>
+<node TEXT="Внешние условия" ID="ID_931100539" CREATED="1685119125326" MODIFIED="1685119144608" VSHIFT_QUANTITY="-30.375 pt"/>
+<node TEXT="Управление рисками" ID="ID_175887985" CREATED="1685119138791" MODIFIED="1685119141037"/>
+</node>
+</node>
+</map>
